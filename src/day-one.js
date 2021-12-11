9c382a2ce555af98db004fdf59fc683d8c0da06d
Richard Bell <richard.bell@instil.co>
2021-12-11 14:44:14 +0000

    A little tidy up on day one.

--- src/day-one.js
+++ src/day-one.js
@@ -1,25 +1,24 @@
 "use strict";
 exports.__esModule = true;
 var fs = require("fs");
-var numberOfTimesDepthIncreases = 0;
-var lastOceanDepth = NaN;
 function findTheNumberOfTimesTheOceanDepthIncreases() {
-    var fileInput = fs.readFile("input.txt", function (err, data) {
+    fs.readFile("input.txt", function (err, data) {
+        var numberOfTimesDepthIncreases = 0;
         var oceanDepthReadings = data.toString().replace(/\r\n/g, '\n').split('\n');
+        var lastOceanDepth = parseInt(oceanDepthReadings[0]);
+        removeStartingOceanDepth(oceanDepthReadings);
         oceanDepthReadings.forEach(function (oceanDepth) {
             var currentOceanDepth = parseInt(oceanDepth);
-            if (Number.isNaN(lastOceanDepth)) {
-                lastOceanDepth = parseInt(oceanDepth);
+            if (lastOceanDepth < currentOceanDepth) {
+                numberOfTimesDepthIncreases++;
             }
-            else {
-                if (lastOceanDepth < currentOceanDepth) {
-                    numberOfTimesDepthIncreases++;
-                }
-                lastOceanDepth = currentOceanDepth;
-            }
+            lastOceanDepth = currentOceanDepth;
         });
         console.log("Number of times size increases is ".concat(numberOfTimesDepthIncreases));
         process.exit(0);
     });
+}
+function removeStartingOceanDepth(oceanDepthReadings) {
+    oceanDepthReadings.splice(0, 1);
 }
 findTheNumberOfTimesTheOceanDepthIncreases();
--- src/day-one.ts
+++ src/day-one.ts
@@ -1,26 +1,29 @@
 import * as fs from "fs";
 
-let numberOfTimesDepthIncreases = 0;
-let lastOceanDepth = NaN
-
 function findTheNumberOfTimesTheOceanDepthIncreases() {
-    let fileInput = fs.readFile("input.txt", function(err, data) {
-        const oceanDepthReadings = data.toString().replace(/\r\n/g,'\n').split('\n');
+    fs.readFile("input.txt", function(err, data) {
+        let numberOfTimesDepthIncreases = 0;
+        let oceanDepthReadings = data.toString().replace(/\r\n/g,'\n').split('\n');
+        let lastOceanDepth = parseInt(oceanDepthReadings[0]);
+        removeStartingOceanDepth(oceanDepthReadings);
         
         oceanDepthReadings.forEach((oceanDepth: string) => {
             let currentOceanDepth = parseInt(oceanDepth);
-            if (Number.isNaN(lastOceanDepth)) {
-                lastOceanDepth = parseInt(oceanDepth);
-            } else {
-                if(lastOceanDepth < currentOceanDepth) {
-                    numberOfTimesDepthIncreases++;
-                }
-                lastOceanDepth = currentOceanDepth;
+            if(lastOceanDepth < currentOceanDepth) {
+                numberOfTimesDepthIncreases++;
             }
+            lastOceanDepth = currentOceanDepth;
         });
+
         console.log(`Number of times size increases is ${numberOfTimesDepthIncreases}`);
         process.exit(0);
     });
 }
 
+function removeStartingOceanDepth(oceanDepthReadings: string[]) {
+    oceanDepthReadings.splice(0, 1);
+}
+
 findTheNumberOfTimesTheOceanDepthIncreases();
+
+
